2742f47df05d6bade474146a45511bc3211e79c2
davephk-finalme-computacionales-81ov3o9n <davephk-finalme-computacionales-81ov3o9n@local>
2018-05-31 20:41:08 +0000

    ej2

--- ejercicio2.py
+++ ejercicio2.py
@@ -6,5 +6,13 @@ import numpy as np
 x = np.int_(np.random.random(100)*10000)
 print(x)
 
+for i in range(0,len(x)):
+    if((x[i] % 2 != 0) and (x[i]%3 !=0 ) and (x[i]%5 != 0) and (x[i]%7 !=0) ):
+        break
+    else:        
+        print (x[i])
+        
+        
+
 
 
