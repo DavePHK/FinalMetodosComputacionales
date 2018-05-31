00f04b81513e82e3f467e012dd4b2bbe0c0bef3e
davephk-finalme-computacionales-81ov3o9n <davephk-finalme-computacionales-81ov3o9n@local>
2018-05-31 20:53:30 +0000

    V1

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,9 +4,25 @@
 # Guarde esta segunda derivada en funcion de x en una grafica llamada 'segunda.png'
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
 
 
+h = 0.001
 
+fxx = fx + h
+d=(fxx-fx)/h
+print(d)
+
+fxxx= d+h
+
+dd = (fxxx-d)/h
+
+plt.figure()
+plt.scatter(x,dd)
+plt.title('Segunda derivada')
+plt.savefig('segunda.png')
--- ejercicio4.py
+++ ejercicio4.py
@@ -7,9 +7,39 @@
 # condiciones iniciales t=0, x=0.5, y=-1.0, z=1.0, hasta t=5.0.
 # Prepare dos graficas con la solucion: de x vs y (xy.png), x vs. z (xz.png) 
 
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+sigma = 10
+beta = 2.67
+rho=28.0
+
+t=0
+x=0.5
+y=-1.0
+z=1.0
+T=5.0
 
 
 
 
 
 
+
+
+
+plt.figure()
+plt.scatter(y,x)
+plt.title('x vs y')
+plt.xlabel('y')
+plt.ylabel('x')
+plt.savefig('xy.png')
+
+plt.figure()
+plt.scatter(z,x)
+plt.title('x vs z')
+plt.xlabel('z')
+plt.ylabel('x')
+plt.savefig('xz.png')
